0f066a0e542cebdfd8d85c608d6bde107f4441f0
Steve-Ji-Y <steveji16@hotmail.com>
2023-03-13 16:28:01 -0400

    learning: project setup

---
+++ src/pages/components/Layout/Navbar/SearchInput.tsx
@@ -0,0 +1,12 @@
+import { Flex, Input } from "@chakra-ui/react";
+import React from "react";
+
+const SearchInput: React.FC<SearchInputProps> = () => {
+  return (
+    <Flex>
+      <Input></Input>
+    </Flex>
+  );
+};
+
+export default SearchInput;
