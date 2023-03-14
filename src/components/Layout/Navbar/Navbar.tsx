10670c70a290bbeae15ea70eefe938b6d8b007db
Steve-Ji-Y <steveji16@hotmail.com>
2023-03-13 20:08:35 -0400

    login header

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
+import SearchInput from "./SearchInput";
+import RightContent from "./RightContent/RightContent";
 
 const Navbar: React.FC = () => {
   return (
@@ -13,9 +15,9 @@ const Navbar: React.FC = () => {
           display={{ base: "none", md: "unset" }}
         />
       </Flex>
-      {/* <Directory />
       <SearchInput />
-      <RightContent /> */}
+      {/* <Directory /> */}
+      <RightContent />
     </Flex>
   );
 };
---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,57 @@
+import { authModalState } from "@/atoms/authModalAtom";
+import {
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+import { useRecoilState } from "recoil";
+import AuthInputs from "./AuthInput";
+
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+    setModalState((prev) => ({ ...prev, isOpen: false }));
+  };
+
+  return (
+    <>
+      <Modal isOpen={modalState.isOpen} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            {modalState.view === "login" && "login"}
+            {modalState.view === "signup" && "Sign Up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              width="70%"
+              border="1px solid red"
+            >
+              {/* <OAuthButtons /> */}
+              <AuthInputs />
+              {/* {/* <ResetPassword /> */}
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AuthModal;
