c0f2cd633af15fe0b779b86e8ebbc999f13e8c89
Steve-Ji-Y <steveji16@hotmail.com>
2023-03-15 15:16:51 -0400

    working on fireStore setup

---
+++ src/components/Layout/Navbar/Directory/Communities.tsx
@@ -0,0 +1,31 @@
+import CreateCommunityModal from "@/components/Modal/Auth/CreateCommunity/CreateCommunityModal";
+import { Flex, MenuItem, Icon } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { GrAdd } from "react-icons/gr";
+
+type CommuntiesProps = {};
+
+const Communities: React.FC<CommuntiesProps> = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <CreateCommunityModal open={open} handleClose={() => setOpen(false)} />
+      <MenuItem
+        width="100%"
+        fontSize="10pt"
+        _hover={{ bg: "gray.100" }}
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        <Flex align="center">
+          <Icon fontSize={20} mr={2} as={GrAdd} />
+          Create Community
+        </Flex>
+      </MenuItem>
+    </>
+  );
+};
+
+export default Communities;
--- src/components/Layout/Navbar/Directory/Directory.tsx
+++ src/components/Layout/Navbar/Directory/Directory.tsx
@@ -1,7 +1,9 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { Menu, MenuList, Flex, Icon, MenuButton, Text } from "@chakra-ui/react";
+import { Autour_One } from "next/font/google";
 import React from "react";
 import { TiHome } from "react-icons/ti";
+import Communities from "./Communities";
 
 const Directory: React.FC = () => {
   return (
@@ -10,19 +12,28 @@ const Directory: React.FC = () => {
         cursor="pointer"
         padding="0px 6px"
         borderRadius={4}
+        mr={2}
+        ml={{ base: 0, md: 2 }}
         _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
       >
-        <Flex align="center">
+        <Flex
+          align="center"
+          justify="space-between"
+          width={{ base: "auto", lg: "200px" }}
+        >
           <Flex align="center">
             <Icon fontSize={24} as={TiHome} mr={{ base: 1, md: 2 }} />
             <Flex display={{ base: "none", lg: "flex" }}>
-              <Text>Home</Text>
+              <Text fontSize="10pt">Home</Text>
             </Flex>
           </Flex>
+          {/* Maximize space between Flex and Cheveron Down */}
           <ChevronDownIcon />
         </Flex>
       </MenuButton>
-      <MenuList></MenuList>
+      <MenuList>
+        <Communities />
+      </MenuList>
     </Menu>
   );
 };
