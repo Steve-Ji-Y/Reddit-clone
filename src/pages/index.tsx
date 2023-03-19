c45de76760cb9289647642c5a26d294cabbe4b28
Steve-Ji-Y <steveji16@hotmail.com>
2023-03-19 10:03:19 -0400

    chore: setup homePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,92 @@
+import { Post } from "@/atoms/postsAtom";
+import CreatePostLink from "@/components/Community/CreatePostLink";
+import PageContentLayout from "@/components/Layout/PageContent";
+import PostItem from "@/components/Posts/PostItem";
+import PostLoader from "@/components/Posts/PostLoader";
+import { auth, firestore } from "@/firebase/clientApp";
+import usePosts from "@/hooks/usePosts";
+import { Stack } from "@chakra-ui/react";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { NextPage } from "next";
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-const inter = Inter({ subsets: ["latin"] });
+const Home: NextPage = () => {
+  const [user, loadingUser] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onSelectPost,
+    onDeletePost,
+    onVote,
+  } = usePosts();
 
-export default function Home() {
-  return <div>Hello</div>;
-}
+  const buildUserHomeFeed = () => {};
+
+  const buildNoUserHomeFeed = async () => {
+    setLoading(true);
+    try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(10)
+      );
+
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+      // set post state
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[],
+      }));
+    } catch (error) {}
+
+    setLoading(false);
+  };
+
+  const getUserPostVotes = () => {};
+
+  // useEffects
+  useEffect(() => {
+    if (!user && !loadingUser) {
+      buildNoUserHomeFeed();
+    }
+  }, [user, loadingUser]);
+
+  return (
+    <PageContentLayout>
+      <>
+        <CreatePostLink />
+        {loading ? (
+          <PostLoader />
+        ) : (
+          <Stack>
+            {postStateValue.posts.map((post) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                onSelectPost={onSelectPost}
+                onDeletePost={onDeletePost}
+                onVote={onVote}
+                userVoteValue={
+                  postStateValue.postVotes.find(
+                    (item) => item.postId === post.id
+                  )?.voteValue
+                }
+                userIsCreator={user?.uid === post.creatorId}
+                homePage
+              />
+            ))}
+          </Stack>
+        )}
+      </>
+
+      <>{/* <Recommendations /> */}</>
+    </PageContentLayout>
+  );
+};
+
+export default Home;
