40cfd51853bfd64a68acadaf4380f27deee2f2e8
Steve-Ji-Y <steveji16@hotmail.com>
2023-03-21 08:31:12 -0400

    fix: TS type issue in [pid]

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -6,21 +6,23 @@ import useCommunityData from "@/hooks/useCommunityData";
 import usePosts from "@/hooks/usePosts";
 import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import About from "@/components/Community/About";
 import Comments from "@/components/Posts/Comments/Comments";
+import PostLoader from "@/components/Posts/PostLoader";
 
 const PostPage: React.FC = () => {
   const { postStateValue, setPostStateValue, onDeletePost, onVote } =
     usePosts();
   const [user] = useAuthState(auth);
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const { communityStateValue } = useCommunityData();
 
-  // console.log(communityStateValue);
-
   const fetchPost = async (postId: string) => {
+    setLoading(true);
+
     try {
       const postDocRef = doc(firestore, "posts", postId);
       const postDoc = await getDoc(postDocRef);
@@ -29,6 +31,8 @@ const PostPage: React.FC = () => {
         selectedPost: { id: postDoc.id, ...postDoc.data() } as Post,
       }));
     } catch (error) {}
+
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -41,24 +45,38 @@ const PostPage: React.FC = () => {
   return (
     <PageContentLayout>
       <>
-        {postStateValue.selectedPost && (
-          <PostItem
-            post={postStateValue.selectedPost}
-            onVote={onVote}
-            onDeletePost={onDeletePost}
-            userVoteValue={
-              postStateValue.postVotes.find(
-                (item) => item.postId === postStateValue.selectedPost?.id
-              )?.voteValue
-            }
-            userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId}
-          />
+        {loading ? (
+          <PostLoader />
+        ) : (
+          <>
+            {" "}
+            {postStateValue.selectedPost && (
+              <>
+                {" "}
+                <PostItem
+                  post={postStateValue.selectedPost}
+                  onVote={onVote}
+                  onDeletePost={onDeletePost}
+                  userVoteValue={
+                    postStateValue.postVotes.find(
+                      (item) => item.postId === postStateValue.selectedPost?.id
+                    )?.voteValue
+                  }
+                  userIsCreator={
+                    user?.uid === postStateValue.selectedPost?.creatorId
+                  }
+                />
+                <Comments
+                  user={user!}
+                  selectedPost={postStateValue.selectedPost}
+                  communityId={
+                    postStateValue.selectedPost?.communityId as string
+                  }
+                />
+              </>
+            )}
+          </>
         )}
-        <Comments
-          user={user!}
-          selectedPost={postStateValue.selectedPost}
-          communityId={postStateValue.selectedPost?.communityId as string}
-        />
       </>
       <>
         {communityStateValue.currentCommunity && (
